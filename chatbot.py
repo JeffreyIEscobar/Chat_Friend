1baee368c9de40987daef7a0d40b940b7409cac6
Jeffrey I. Escobar <Jeffreyianescobar@gmail.com>
2023-08-31 22:27:15 -0400

    New files changed chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, jsonify
+import openai
+
+app = Flask(__name__)
+
+# Set your OpenAI API key
+openai.api_key = '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'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_response', methods=['POST'])
+def get_response():
+    user_input = request.form['user_input']
+    response = ask_openai(user_input)
+    return jsonify({'response': response})
+
+def ask_openai(question):
+    messages = [{'role': 'system', 'content': 'You are a chatbot.'}, {'role': 'user', 'content': question}]
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    return response.choices[0].message['content']
+
+if __name__ == '__main__':
+    app.run(debug=True)
